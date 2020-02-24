65f6f9a134905db3a1f5a4e870d07c5f5889a73c
Clayton Moore <cmoorefour@gmail.com>
2020-02-24 13:50:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-function dwarfRollCall(dwarves) {
 var x = []
+
+function dwarfRollCall(dwarves) {
 for (var i = 0; i < dwarves.length;  i++) {
  x += `${[i + 1]}` + '. ' + `${dwarves[i]}` + ' '
 }
  return x;
 }
 
-function summonCaptainPlanet(planeteerCalls) {
   var array = []
+
+
+function summonCaptainPlanet(planeteerCalls) {
   for (var i=0;i<planeteerCalls.length;i++) {
   array.push(`${planeteerCalls[i].toUpperCase()}!`)
 }
 return array
 }
 
+
+  var array = []
+
 function longPlaneteerCalls(elements){
-  var x = []
+
+
  for (var i = 0; i < elements.length;  i++){
   if (elements[i].length > 4){
   return true;
@@ -24,9 +31,12 @@ function longPlaneteerCalls(elements){
   return false;
 }
 
+
+var x = []
+
 function findTheCheese(groceries){
 var typesOfCheese = ['cheddar', 'gouda', 'camembert']
-var x = []
+
 for (var i = 0; i < typesOfCheese.length;  i++){
 for (var j = 0; j < groceries.length; j++){
   if (typesOfCheese[i] == groceries[j]){
